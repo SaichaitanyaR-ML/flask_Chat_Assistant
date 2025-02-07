10fc06816986ddc44af8067d0a926ebed3592575
SaichaitanyaR-ML <saichaitanyareturi@gmail.com>
2025-02-07 22:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render uses a dynamic port
+    port = int(os.environ.get("PORT", 10000))  # Render uses a dynamic port
     app.run(host="0.0.0.0", port=port)
